afb2e7df19f9e79acae1593da1ec32542000c363
Jaeyoung9999 <ehrlrkr@gmail.com>
2025-07-05 11:43:24 +0900

    [FEAT] prettier, zustand 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
 function App() {
-
   return (
     <div className="bg-blue-500 text-white p-4 rounded-lg">
       <h1 className="text-2xl font-bold">Hello Tailwind!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
